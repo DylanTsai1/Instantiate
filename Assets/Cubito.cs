d53d0fadc7da94453efba2f022c1f4ea42ef0ec7
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-05-31 08:01:00 -0300

    Initial commit

---
+++ Assets/Cubito.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubito : MonoBehaviour
+{  
+ public GameObject objectClone; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Instantiate()
+    {
+        int i = 0;
+        while(i<3)
+        Instantiate(objectClone);
+        i++; 
+        
+
+    }
+}
